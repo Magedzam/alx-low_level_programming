c93b93e69c3ba1711f783ab05c47341cff138222
Magedzam <magedzamzamy@gmail.com>
2023-07-13 19:40:27 -0700

    Maged

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry Point
+ *
+ * Description: A C programm that prints using printf function
+ *
+ * Return: Always 0 (success)
+ *
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}	
