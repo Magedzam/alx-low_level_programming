f0bb15ca93782ae403dc4f90054fc1d1ce16fd60
Magedzam <magedzamzamy@gmail.com>
2023-07-13 19:35:08 -0700

    Maged

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print with puts function
+ *
+ * Return always 0 (success)
+ *
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
