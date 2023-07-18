23070a78ceb0d24b9adfed9368f4914a2680ba22
Magedzam <magedzamzamy@gmail.com>
2023-07-17 19:40:49 -0700

    Maged

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point.
+ *
+ * Description: printing alphabet in lower case.
+ *
+ * Return: Always 0 (Success).
+ *
+*/
+
+int main(void)
+{	
+	for (char ch= 'a'; a <= 'z', ch++)
+	putchar(ch);
+	return (0);
+}	
