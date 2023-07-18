e07229eee651f1ac2857e6b2b8cd4d2fccfce59a
Magedzam <magedzamzamy@gmail.com>
2023-07-17 19:01:52 -0700

    Maged

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,11 +7,11 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0){
-		printf("is positive\n");
-	}else if (n=0){
-		printf("is zero\n");
-	}else{
-		printf("is negative\n");
-	}return (0);
+	if(n>0)
+		printf("%i is positive\n", n);
+	else if (n=0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
+	return (0);
 }
