bcf8e868e9abe0b8dcb67865c73491d850da228c
Magedzam <magedzamzamy@gmail.com>
2023-07-17 20:02:22 -0700

    Maged

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point.
+ *
+ * Description: prints all the alphabet in lowercase except q and e.
+ *
+ * Return: Always 0 (Success).
+ *
+*/
+
+
+int main(void)
+{
+	char ch = 'a';
+
+	while(ch <= 'z' && ch != q && ch != e)
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
