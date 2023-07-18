a91dfbffc5fe28bd26497019548336b24c935144
Magedzam <magedzamzamy@gmail.com>
2023-07-18 15:43:27 -0700

    Maged

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point.
+ *
+ * Description: A program that prints a word.
+ *
+ * Return: Always 0 (Success).
+ *
+*/
+
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}	
