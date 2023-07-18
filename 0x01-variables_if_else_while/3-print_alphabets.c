398b2d88d0da5bce80e127126d7423418b2ffa77
Magedzam <magedzamzamy@gmail.com>
2023-07-17 19:53:42 -0700

    Maged

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,12 +14,17 @@ int main(void)
 	char ch1= 'a';
 	char ch2= 'A';
 
-	while (ch1 <= 'a', ch2 <= 'A')
+	while (ch1 <= 'a')
 	{
-		putchar(ch1, ch2);
+		putchar(ch1);
 		ch1++;
-		ch2++;
 	}
+
+	while (ch2 <= 'A')
+	{
+		putchar(ch2);
+		ch2++;
+	}	
 	putchar('\n');
 	return (0);
 }
