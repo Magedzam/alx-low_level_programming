f7b846aee9f01d3aeaf63fbba96d987ab3418309
Magedzam <magedzamzamy@gmail.com>
2023-07-19 12:46:38 -0700

    Maged

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,22 @@
 #include "main.h"
 
 
-/***/
+/**
+ * print_last_digit - print last digit of a number
+ *
+ * @a: take integer numper input
+ *
+ * Return: Lastdigit
+ */
 
 
 
 int print_last_digit(int a)
 {
-	a = a % 10;
-	return (a);
+	if ( a < 0)
+		lastdigit = -1 * ( a % 10);
+	lastdigit = a % 10;
+	_putchar(lastdigit);
+	return (lastdigit);
 return (0);
 }
