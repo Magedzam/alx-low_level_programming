a66a1a2760d9aafda1ee9217f2cbcc38fa0ff884
Magedzam <magedzamzamy@gmail.com>
2023-07-19 05:39:54 -0700

    Maged

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,13 @@
-#include "main.h'
-#include <stdio.h>
+#include "main.h"
+
+
 
 void print_alphabet(void)
 {
-	char ch = 'a';
+	char ch;
 
-	for (ch='a' ; ch <= 'z'; ch++)
-		putchar(ch)
-	putchar('\n');
+	for(ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
 	return (void);
 }	
-
-
-
