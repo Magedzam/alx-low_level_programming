d76d8e9e5407eb58371036e3c13baee81fd6d83b
Magedzam <magedzamzamy@gmail.com>
2023-07-19 06:19:03 -0700

    Maged

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@
 
 void print_alphabet_x10(void)
 {
-        char ch;
-        int i;
+	char ch;
+	int i;
 
-        for (i = 0; i <= 9; i++)
-        {
-                 for (ch = 'a'; ch <= 'z'; ch++)
-                         _putchar(ch);
-                 _putchar('\n');
-        }
+	for (i = 0; i <= 9; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
 }
