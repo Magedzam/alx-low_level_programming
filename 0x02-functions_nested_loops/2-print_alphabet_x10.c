b1f3bda4b78b24557df25ce786311d3bc1b1e15d
Magedzam <magedzamzamy@gmail.com>
2023-07-19 06:05:22 -0700

    Maged

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - utilize on the _putchar function to print 
+ *                              the alphabet a-z 10 times
+*/
+
 void print_alphabet_x10(void)
 {
 	char ch;
