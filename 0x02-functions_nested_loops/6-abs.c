b3c23ec4253ccf89c8012592f64137984a8df9a4
Magedzam <magedzamzamy@gmail.com>
2023-07-19 07:49:57 -0700

    Maged

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,11 +6,7 @@
 
 int _abs(int n)
 {
-	if (n > 0)
-		printf("%d", n);
-	else if (n == 0)
-		printf("%d", n);
-	else
-		printf("%d", n*-1);
-return (0);
+	if (n < 0)
+		n = (-1) * n
+	return (n);		
 }
