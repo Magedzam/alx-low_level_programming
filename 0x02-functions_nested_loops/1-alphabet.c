49162fbb9edeb7edfce5c3b1c99a662ab1a80fde
Magedzam <magedzamzamy@gmail.com>
2023-07-19 05:46:14 -0700

    Maged

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *
- * Description: A Function that prints the alphabet in lowercase.
+ * Description: A Function print_alphabet utilizes on the _putchar function
+ * to print the alphabet form a-z
  *
 */
 
