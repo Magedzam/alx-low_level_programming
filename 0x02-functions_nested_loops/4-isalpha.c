f2736c61c1fbf4b3823e6c359080f621843affec
Magedzam <magedzamzamy@gmail.com>
2023-07-19 07:10:45 -0700

    Maged

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha: function to check if the input is character
+ *                      in lowercaser or uppercase
+ *
+ * @c: takes input from other functions.
+ *
+ * Return: 1 is c if true otherwise 0.
+*/
+
+
+
+int _isalpha(int c)
+{
+	if ( c >= 65 && c <= 90 && c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
