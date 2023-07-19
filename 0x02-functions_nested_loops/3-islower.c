5864cace7a22b3c990cfbce940c4f6184a00fbbd
Magedzam <magedzamzamy@gmail.com>
2023-07-19 06:56:35 -0700

    Maged

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int _islower - function to check if the character is lowercase
+ *
+*/
+
+int _islower(int c)
+{
+	char ch;
+
+	if (islower(ch))
+		return (1);
+	return (0);
+}	
