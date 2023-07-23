b80b58767ac6069398e210a05af5915da2dccee8
Magedzam <magedzamzamy@gmail.com>
2023-07-23 15:17:33 -0700

    Maged

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
 
+/**
+ * main - Entry Point.
+ *
+ * Description: print a-z reverse.
+ *
+ * Return: Always 0 (Success).
+ *
+*/
+
 int main(void)
 {
 	int i;
 
 	for (i = 122; i >= 97; i--)
+
 	{
 		putchar(i);
 	}
