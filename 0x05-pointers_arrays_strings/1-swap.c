0f0c963e9ae20b7927c1b674a51b5f407fcc5e2c
Magedzam <magedzamzamy@gmail.com>
2023-07-24 18:24:45 -0700

    Maged

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,17 @@
 #include "main.h"
 
+/**
+ * swap_int - swaps the values of two integrs
+ *                   using two input parameters
+ *
+ * @a: int parameter 1
+ * @b; int parameter 2
+ *
+ * Return: Nothing
+ *
+*/
+
+
 
 void swap_int(int *a, int *b)
 {
