30de7988e6f0429b3c99cc994ff45172983e8273
Magedzam <magedzamzamy@gmail.com>
2023-07-25 19:00:44 -0700

    Maged

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+
+/**
+ * *_strcpy - copies the string pointed to be src
+ * @dest: char type string
+ * @src: char type string
+ * the buffer pointed to be dest
+ * Return: Nothing
+*/
+
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1; /* to include null byte */
+
+	do {
+		i++;
+		src[i] = dest[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
