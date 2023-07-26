becdae7783888fedd5a8b40ce119e3756a715fec
Magedzam <magedzamzamy@gmail.com>
2023-07-25 17:42:13 -0700

    Maged

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,20 @@
 #include "main.h"
 
+/**
+ * puts2 - prints every character of a string
+ *
+ * @str: string parameter input.
+ *
+ * Return: Nothing
+ *
+*/
+
+
+
 void puts2(char *str)
 {
 	int i = 0;
-	
+
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
