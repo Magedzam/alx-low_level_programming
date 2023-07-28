99efbd94270cf0aff949a564f90ad9666e1ed3cd
Magedzam <magedzamzamy@gmail.com>
2023-07-28 02:58:44 -0700

    Maged

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,14 @@ char *_strcat(char *dest, char *src)
 	int c = 0;
 	int c1;
 
-	while (dest[c])
+	while (dest[c] != '\0')
 	{
 		c++;
 	}
 	for (c1 = 0; src[c1]; c1++)
 	{
-		c++;
 		dest[c] = src[c1];
+		c++;
 	}
 	return (dest);
 }
