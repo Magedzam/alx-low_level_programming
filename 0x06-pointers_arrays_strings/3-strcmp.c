dc8fa3f6b8975a16b26254a97ff2ebbe163c9d31
Magedzam <magedzamzamy@gmail.com>
2023-07-28 07:55:23 -0700

    Maged

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,6 +6,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int equal = 0;
 
-	equal = ((int)*s1 - (int)*s2);
+	if (*s1 != *s2)
+	{
+		equal = ((int)*s1 - (int)*s2);
+	}
+	else equal = 0;
 	return (equal);
 }
