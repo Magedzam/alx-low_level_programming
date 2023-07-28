142b40381fba95ea0dad47c8437d3ab8338e106c
Magedzam <magedzamzamy@gmail.com>
2023-07-28 03:04:38 -0700

    Maged

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,6 +2,16 @@
 
 
 
+/**
+ * _strcat - function that concatenates two strings
+ *
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ *
+ * Return: Pointer to resulting string @dest
+ *
+*/
+
 
 char *_strcat(char *dest, char *src)
 {
@@ -14,8 +24,8 @@ char *_strcat(char *dest, char *src)
 	}
 	for (c1 = 0; src[c1]; c1++)
 	{
-		c++;
 		dest[c] = src[c1];
+		c++;
 	}
 	return (dest);
 }
