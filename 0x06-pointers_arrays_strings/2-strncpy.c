0bd31318c891aaf08f4ebe79c289e71a137cc38f
Magedzam <magedzamzamy@gmail.com>
2023-07-28 04:43:25 -0700

    Maged

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -20,10 +20,10 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[c] = src[c];
 	}
-	while (i < n)
+	while (c < n)
 	{
-		dest[i] = '\0';
-		i++;
+		dest[c] = '\0';
+		c++;
 	}
 	return (dest);
 }
