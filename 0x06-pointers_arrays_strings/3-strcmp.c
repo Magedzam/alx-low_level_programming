c8e80662cec390e39ed1f230db6d2f104b6aa507
Magedzam <magedzamzamy@gmail.com>
2023-07-28 06:23:38 -0700

    Maged

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,22 +4,8 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int c1;
-	int c2;
+	int equal = 0;
 
-	for (c1 = 0; s1[c1] != '\0'; c1++)
-		;
-	for (c2 = 0; s2[c2] != '\0'; c2++)
-		;
-	if (c1 > c2)
-	{
-		printf("15");
-	}
-	else if (c1 < c2)
-	{
-		printf("-15");
-	}
-	else printf("0");
-
-	return (0);
+	equal = ((int)*s1 - (int)*s2);
+	return (equal);
 }
