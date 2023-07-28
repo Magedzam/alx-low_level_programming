84bd0d53ad12064fad40a63979edd33811da393d
Magedzam <magedzamzamy@gmail.com>
2023-07-28 03:20:38 -0700

    Maged

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,7 +3,7 @@
 
 
 /**
- * _strncat - a function that concatenates two strings 
+ * _strncat - a function that concatenates two strings
  *
  * @dest: pointer to destination input
  * @src: pointer to source input
