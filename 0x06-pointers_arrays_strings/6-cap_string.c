64406d5b16d7f03cafe70bf0946a725ac522c47a
Magedzam <magedzamzamy@gmail.com>
2023-07-29 06:36:37 -0700

    Maged

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+char *cap_string(char *s)
+{
+	char no[] = {'\n', '\t', ',', ' ', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i1;
+	char *ss = s;
+
+	while (*s != '\0')
+	{
+		for (i1 = 0; i1 < 13; i1++)
+		{
+			if (*s == no[i1])
+			{
+				s++;
+				*s = *s - 32;
+				break;
+			}
+		}
+		s++;
+		return (ss);
+	}
+}
