48db4b4e35ccd04eec95306766328e6ae50fc90c
Magedzam <magedzamzamy@gmail.com>
2023-07-29 02:19:36 -0700

    Maged

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,13 +14,13 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	int j;
+	int c
 	int t;
 
-	for (i = 0; j = (n - 1); i <= j; i++; j--)
+	for (i = , c = (n - 1); i <= c; i++, c--)
 	{
 		t = a[i];
-		a[i] = a[j];
-		a[j] = t;
+		a[i] = a[c];
+		a[c] = t;
 	}
 }
