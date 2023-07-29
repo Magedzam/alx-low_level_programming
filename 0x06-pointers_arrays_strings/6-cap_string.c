5074bd1a37dbcb6d1a329bbee3e84d0025f030a3
Magedzam <magedzamzamy@gmail.com>
2023-07-29 07:02:22 -0700

    Maged

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,67 @@
 #include "main.h"
 
 
-char *cap_string(char *s)
-{
-	char no[] = {'\n', '\t', ',', ' ', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-	int i1;
-	char *ss = s;
+/**
+ * islower - determines whether ascii is lowercase
+ * @c: character
+ * Return: 1 if true, 0 if false
+ *
+*/
 
-	while (*s != '\0')
+int islower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+
+
+
+/**
+ * isdelimiter - determines whether ascii is a delimiter
+ * @c: character
+ * Return: 1 if true, 0 if false
+ *
+*/
+
+int isdelimiter(char c)
+{
+	int i;
+	char no[] = " \t\n,.?;"{}()!";
+
+	for (i = 0; i < 13; i++)
 	{
-		for (i1 = 0; i1 < 13; i1++)
+		if (c == no[i])
 		{
-			if (*s == no[i1])
-			{
-				s++;
-				*s = *s - 32;
-				break;
-			}
+			return (1);
 		}
+	return (0);
+	}
+}
+
+
+/**
+ * *cap_string - capitalizes all words of a string
+ * @s: input string
+ * Return: strings with capatlized words
+ *
+*/
+
+char *cap_string(char *)
+{
+	char *ss = s;
+	int foundDelimit = 1;
+
+	while (*s)
+	{
+		if (isdelimiter(*s))
+			foundDelimit = 1;
+		else if
+		{
+			*s -= 32;
+			foundDelimit = 0;
+		}
+		else
+			foundDelimit = 0;
 		s++;
 	}
 	return (ss);
