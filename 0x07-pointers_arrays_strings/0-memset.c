87931057d6d99c317c1917c220c13ee1c793c272
Magedzam <magedzamzamy@gmail.com>
2023-07-31 19:25:39 +0300

    Maged

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	int i;
+	char *ptr = s;
+
+	while (*s != '\0')
+	{
+		for (i = 0; i <= n; i++)
+		{
+			s[i] = b;
+		}
+		s++;
+	}
+	return (ptr);
+}
