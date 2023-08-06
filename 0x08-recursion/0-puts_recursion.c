42f0b5a19cec3ff003d6e8e49ccbb3da1fe721af
Magedzam <magedzamzamy@gmail.com>
2023-08-06 20:57:28 +0300

    Maged

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
 
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to a string
+ * Return: nothing
+*/
+
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
